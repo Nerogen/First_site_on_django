5081dee0654a2c6df93ae4402ac86d9dfb2441f7
Nero <72101790+PishchDaniil@users.noreply.github.com>
2021-12-24 20:55:07 +0300

    Update settings.py

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2h5f3wek0ca+lu%(pl9r4wqj070_4ah-o^^1l8*!e9=qcs%!fw'
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
